52b99c45fa0d0b1d51457491cb497b47320bd693
Almaz Ahmad <almazahmad03@gmail.com>
2018-08-15 18:44:17 +0100

    Create Rails Application with home and about pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def home
+    # defining a home action
+  end
+
+  def about
+    # defining an about action
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #
+  # for the pages controller
+  root'pages#home'        # this is the home action which is now also the homepage
+
+
+  get 'about', to: 'pages#about'      # this is the about aciton
+
+end
