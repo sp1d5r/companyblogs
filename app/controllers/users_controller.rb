39221441058fcec1c4793e628190e5ee76533971
Almaz Ahmad <almazahmad03@gmail.com>
2018-08-18 21:04:02 +0100

    fixed the sign up issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
       flash[:success] = "Welcome to Vortex Blog #{@user.username}"
