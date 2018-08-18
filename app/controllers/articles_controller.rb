22175a888ef1040a1168f7dfc234ebbe55e6914e
Almaz Ahmad <almazahmad03@gmail.com>
2018-08-18 16:48:14 +0100

    A whole bunch of styling done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -53,6 +53,7 @@ class ArticlesController < ApplicationController
         def set_article
             @article = Article.find(params[:id])
         end
+
         def article_params
             params.require(:article).permit(:title, :descriptions)
         end
